a21e94c74fe86f05c83daa796d492113b70470df
Mimi Flynn <mimiflynn@gmail.com>
2015-08-21 16:44:53 -0600

    initial grunt setup with web pack and lecture notes update

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      build: ['client/js/app.js']
+    },
+    concurrent: {
+      target: {
+        tasks: ['watch', 'connect:server'],
+        options: {
+          logConcurrentOutput: true,
+          limit: 5
+        }
+      }
+    },
+    webpack: {
+      build: {
+        entry: "./source/scripts/app.js",
+        output: {
+            path: "client/js/",
+            filename: "app.js",
+        },
+        stats: {
+            // Configure the console output
+            colors: false,
+            modules: true,
+            reasons: true
+        }
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'source/scripts/**/*.js'],
+      options: {
+        globals: {
+          console: true
+        },
+        additionalSuffixes: ['.js', '.jsx']
+      }
+    },
+    watch: {
+      dev: {
+        files: ['<%= jshint.files %>', 'source/**/*.jsx', 'source/sass/**/*.scss', 'source/**/*.js', 'app/views/**/*.jsx'],
+        tasks: ['jshint', 'compass', 'browserify']
+      },
+    },
+    connect: {
+      server: {
+        port: 1337,
+        base: 'public'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-jsxhint');
+  grunt.loadNpmTasks('grunt-connect');
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'webpack']);
+
+};
--- lecture-notes.md
+++ lecture-notes.md
@@ -1,4 +1,59 @@
+Talking Points
+- server side views must be set to .jsx files - aka react templates
+- explain require and grunt and webpack and babel for es6
+- leave out es6 for easier intro to junior devs?
+
 # Isomophic React
 
-- server side views must be set to .jsx files - aka react templates
-- 
+## Assumptions
+- familiarity wth javascript only in the realm of front end development
+- no prior backend JS experience although perhaps with some build tools
+
+# Javascript modules
+
+A handy chunk of javascript usually kept in its own individual file and loaded via require() statements.
+
+## require()
+
+Require allows a dev to include a library (module) in their file.
+
+```
+var _ = require('underscore');
+
+module.exports = {
+
+};
+```
+
+# Tools
+
+## npm
+
+Dependencies to external libraries and tools are loaded into the project via `npm`. 
+
+```
+npm install
+```
+
+## Grunt
+
+Runs web server as well.
+
+## Webpack
+
+https://webpack.github.io
+
+Bundles modules into a big file to load client side.
+
+https://webpack.github.io/docs/configuration.html
+
+webpack suggests using its own server thingy, but since we are going to be running a backend api 
+
+## Babel
+
+https://babeljs.io
+
+## References
+
+https://robots.thoughtbot.com/setting-up-webpack-for-react-and-hot-module-replacement
+
