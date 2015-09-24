f0b74ec5909c89ef04e136819a7e2925e5d3e1bd
Mimi Flynn <mimiflynn@gmail.com>
2015-09-24 08:20:49 -0600

    refactor login screen to use form wrapper

--- components/users/login.js
+++ components/users/login.js
@@ -1,10 +1,11 @@
 var React = require('react');
 
 var DefaultLayout = require('../layouts/default');
+var FormWrapper = require('../layouts/wrappers/form');
 
 module.exports = React.createClass({
   propTypes: {
-    csrf_token: React.PropTypes.string.required,
+    csrf_token: React.PropTypes.string,
     title: React.PropTypes.string
   },
 
@@ -17,27 +18,22 @@ module.exports = React.createClass({
   render: function () {
     return (
       <DefaultLayout title={this.props.title}>
-        <form action="/users/session" method="post" role="form" className="form-horizontal">
-          <input type="hidden" name="_csrf" value={ this.props.csrf_token } />
-
-          <p className="col-sm-offset-2 error">
-            { this.state.message }
-          </p>
-
+        <h2 className="col-sm-offset-2 error">
+          { this.state.message }
+        </h2>
+        <FormWrapper actionUrl="/users/session" httpMethod="post" csrf_token={ this.props.csrf_token } >
           <div className="form-group">
             <label for="email" className="col-sm-2 control-label">Email</label>
             <div className="col-sm-10">
               <input id="email" className="form-control" type="email" placeholder="Email" name="email" />
             </div>
           </div>
-
           <div className="form-group">
             <label for="password" className="col-sm-2 control-label">Password</label>
             <div className="col-sm-10">
               <input id="password" className="form-control" type="password" placeholder="Password" name="password" />
             </div>
           </div>
-
           <div className="form-group">
             <div className="col-sm-offset-2 col-sm-10">
               <button className="btn btn-primary" type="submit">Log in</button>
@@ -45,7 +41,7 @@ module.exports = React.createClass({
               <a href="/signup" className="show-signup">Sign up</a>
             </div>
           </div>
-        </form>
+        </FormWrapper>
       </DefaultLayout>
     )
   }
